b9deba32e649dadc61eef850e2a5299767da1fe4
Garvey Zhu <18550582063@163.com>
2016-12-08 19:32:33 +0800

    创建 develop 分支

--- src/main/java/com/garvey/entity/TestBean.java
+++ src/main/java/com/garvey/entity/TestBean.java
@@ -4,4 +4,8 @@ package com.garvey.entity;
  * Created by Administrator on 2016/12/8 0008.
  */
 public class TestBean {
+
+    public static void main(String[] args) {
+        System.out.print("123");
+    }
 }
