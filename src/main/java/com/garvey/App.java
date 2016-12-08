9175025e3a26a51a052ef5ee16f9b1fa358b1985
garvey.zhu <garvey.zhu@movit-tech.com>
2016-12-08 15:52:05 +0800

    第一次提交

---
+++ src/main/java/com/garvey/App.java
@@ -0,0 +1,13 @@
+package com.garvey;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
